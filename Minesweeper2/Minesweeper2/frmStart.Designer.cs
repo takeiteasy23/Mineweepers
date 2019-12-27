8a178de856bcd7549288421134183a6d6e25d8ce
takeiteasy23 <ex15606ct14@gmail.com>
2019-12-27 09:55:44 +0800

    Update frmStart.Designer.cs

--- Minesweeper2/Minesweeper2/frmStart.Designer.cs
+++ Minesweeper2/Minesweeper2/frmStart.Designer.cs
@@ -150,6 +150,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.start);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "frmStart";
             this.Text = "Minesweeper2";
             this.ResumeLayout(false);
